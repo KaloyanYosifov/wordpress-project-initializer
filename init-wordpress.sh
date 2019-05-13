8b75b19aa8b582828277f8b477f1f5500e625835
Kaloyan Yosifov <kyosifov@2create.studio>
2019-05-13 18:23:39 +0300

    Add getopts when executing script

--- init-wordpress.sh
+++ init-wordpress.sh
@@ -10,9 +10,30 @@ checkIfbinaryExists()
         exit 1
     fi
 }
+usage() { echo "Usage: $0 [-s <45|90>] [-p <string>]" 1>&2; exit 1; }
 
 # check the wp binary
 checkIfbinaryExists wp "Please install wp-cli!"
 
 # check mysql binary
 checkIfbinaryExists mysql "Please install mysql!"
+
+# check if we have arguments less than 0
+if [ $# -lt 1 ]; then
+    echo "Please enter at least (-d) the directory parameter. With the path you want your project to be installed in!"
+    exit 1
+fi
+
+# declare the parameters array
+declare -a paramtersArray
+
+while getopts ":d:" option
+do
+    echo $option ${OPTARG}
+done
+
+# # loop through all parameters
+# for parameter in $*
+# do
+
+# done
